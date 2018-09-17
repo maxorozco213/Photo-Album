c0bdba2ed75ca72e21e9e15614ca7dffe77d2a31
Maximilian Orozco <maxorozco213@gmail.com>
2018-09-17 13:57:14 -0700

    Creating app intro page

---
+++ Router.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View} from 'react-native';
+import { StackNavigator } from "react-navigation";
+
+import Screen1 from './src/Screen1';
+
+const navStack = StackNavigator({
+  Screen1: { screen: Screen1 }
+});
+
+class Router extends Component {
+  static navigationOptions = { header: null, headerMode: 'none' }
+  render() {
+    const screenProps = this.props;
+    return (
+      <navStack screenProps={screenProps} />
+    );
+  }
+}
+
+export default Router;
